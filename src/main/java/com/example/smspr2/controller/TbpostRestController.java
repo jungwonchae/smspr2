de7a309ac84d8ee5268743d773f649008fab64d5
jungwonchae <jungwonchae@Jungwons-MacBook-Pro.local>
2024-07-17 15:21:36 +0900

    added update function

--- src/main/java/com/example/smspr2/controller/TbpostRestController.java
+++ src/main/java/com/example/smspr2/controller/TbpostRestController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "1-1. 게시글 API 안내",
         description = "게시글 관련 기능 정의한 RestController.")
@@ -32,6 +29,17 @@ public class TbpostRestController {
     @PostMapping("")
     public ResponseEntity<TbpostDto.CreateResDto> create(@Valid @RequestBody TbpostDto.CreateReqDto param){
         return ResponseEntity.status(HttpStatus.CREATED).body(tbpostService.create(param));
+    }
+
+    @Operation(summary = "게시글 수정",
+            description = "게시글 수정 컨트롤러 (사용자만 접근 가능) <br />"
+                    + "@param TbpostDto.UpdateReqDto <br />"
+                    + "@return HttpStatus.CREATED(201) ResponseEntity\\<TbpostDto.CreateResDto\\> <br />"
+                    + "@exception 필수 파라미터 누락하였을 때 등 <br />"
+    )
+    @PutMapping("")
+    public ResponseEntity<TbpostDto.CreateResDto> update(@Valid @RequestBody TbpostDto.UpdateReqDto param){
+        return ResponseEntity.status(HttpStatus.OK).body(tbpostService.update(param));
     }
 
 
--- src/main/java/com/example/smspr2/repository/TbpostRepository.java
+++ src/main/java/com/example/smspr2/repository/TbpostRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TbpostRepository extends JpaRepository<Tbpost, String> {
+public interface TbpostRepository extends JpaRepository<Tbpost, String> {
 }
