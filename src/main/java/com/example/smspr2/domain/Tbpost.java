3ef7cc65feb2bd5a2855706418142d4c349029d9
jungwonchae <jungwonchae@Jungwons-MacBook-Pro.local>
2024-07-17 14:49:50 +0900

    modified Tbpost and TbpostService interface

--- src/main/java/com/example/smspr2/domain/Tbpost.java
+++ src/main/java/com/example/smspr2/domain/Tbpost.java
@@ -1,13 +1,16 @@
 package com.example.smspr2.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Table
+@Table(indexes = { //table annotation 에 대한 상세한 내용
+        @Index(columnList = "deleted")
+        ,@Index(columnList = "process")
+        ,@Index(columnList = "createdAt")
+        ,@Index(columnList = "modifiedAt")
+}) //인덱스 키가 유니크 키(여러개를 한번에 설정할때) 등을 설정할 수 있음
 @Entity
 public class Tbpost extends AuditingFields {
 
@@ -16,8 +19,8 @@ public class Tbpost extends AuditingFields {
 //    @Setter private String createdAt;
 //    @Setter private String modifiedAt;
 
-    @Setter private String title;
-    @Setter private String author;
-    @Setter private String content;
+    @Setter @Column private String title;
+    @Setter @Column private String author;
+    @Setter @Column private String content;
 
 }
--- src/main/java/com/example/smspr2/service/TbpostService.java
+++ src/main/java/com/example/smspr2/service/TbpostService.java
@@ -1,4 +1,9 @@
 package com.example.smspr2.service;
 
+import com.example.smspr2.dto.TbpostDto;
+
 public interface TbpostService {
+    TbpostDto.CreateResDto create(TbpostDto.CreateReqDto param);
+    TbpostDto.CreateResDto update(TbpostDto.UpdateReqDto param);
+    TbpostDto.SelectResDto detail(TbpostDto.SelectReqDto param);
 }
