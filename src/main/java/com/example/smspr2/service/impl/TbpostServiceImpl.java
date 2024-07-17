8273c221f36d1059adf7c6f836f5888e4570c195
jungwonchae <jungwonchae@Jungwons-MacBook-Pro.local>
2024-07-17 15:48:18 +0900

    modified TbpostServiceImpl

--- src/main/java/com/example/smspr2/service/impl/TbpostServiceImpl.java
+++ src/main/java/com/example/smspr2/service/impl/TbpostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.smspr2.service.impl;
 
+import com.example.smspr2.domain.Tbpost;
 import com.example.smspr2.dto.TbpostDto;
 import com.example.smspr2.repository.TbpostRepository;
 import com.example.smspr2.service.TbpostService;
@@ -15,17 +16,34 @@ public class TbpostServiceImpl implements TbpostService {
 
     @Override
     public TbpostDto.CreateResDto create(TbpostDto.CreateReqDto param) {
-
-        return null;
+        Tbpost tbpost = tbpostRepository.save(param.toEntity());
+        return tbpost.toCreateResDto();
     }
 
     @Override
     public TbpostDto.CreateResDto update(TbpostDto.UpdateReqDto param) {
-        return null;
+        Tbpost tbpost = tbpostRepository.findById(param.getId()).orElseThrow(()->new RuntimeException("no data"));
+        if(param.getTitle() != null){
+            tbpost.setTitle(param.getTitle());
+        }
+        if(param.getContent() != null){
+            tbpost.setContent(param.getContent());
+        }
+        tbpostRepository.save(tbpost);
+        return tbpost.toCreateResDto();
     }
 
     @Override
     public TbpostDto.SelectResDto detail(TbpostDto.SelectReqDto param) {
-        return null;
+        Tbpost tbpost = tbpostRepository.findById(param.getId()).orElseThrow(()->new RuntimeException("no data"));
+        TbpostDto.SelectResDto selectResDto =  TbpostDto.SelectResDto.builder()
+                .id(tbpost.getId())
+                .createdAt(tbpost.getCreatedAt() + "")
+                .deleted(tbpost.getDeleted())
+                .title(tbpost.getTitle())
+                .author(tbpost.getAuthor())
+                .content(tbpost.getContent())
+                .build();
+        return selectResDto;
     }
 }
